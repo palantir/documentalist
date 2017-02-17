6ebbe4c60d44b662f137da7b241e3d3a2292a044
Bill Dwyer <bdwyer@palantir.com>
2017-02-17 16:26:40 -0500

    navigable test

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,7 @@
 import { IPageData } from "./page";
-import { ITreeNode } from "./plugins/markdown";
+import { ITreeEntry, ITreeNode } from "./plugins/markdown";
 import { IInterfaceEntry } from "./plugins/typescript";
+import { ITag, isTag, ContentNode } from "./";
 
 // explicitly export all the interfaces
 export { IBlock } from "./";
@@ -18,4 +19,70 @@ export interface IMarkdownDocs {
 export interface IDocumentalistData {
     docs: IMarkdownDocs;
     ts: IInterfaceEntry[];
+}
+
+export type INavigableVisitor = (navigable: ITreeEntry) => void;
+
+export function visitNavigables(data: IDocumentalistData, initialPage: IPageData,  visitor: INavigableVisitor) {
+    if (initialPage.contents != null) {
+        initialPage.contents.forEach((node: ContentNode, i: number) => {
+            if (isTag(node)) {
+                if (node.tag == "page") {
+                    const subpage = data.docs.pages[node.value as string];
+                    visitor({
+                        title: "top " + subpage.reference,
+                        reference: subpage.reference
+                    });
+                    visitNavigables(data, subpage, visitor);
+                }
+                if (i != 0 && node.tag.match(/^#+$/)) {
+                    visitor({
+                        title: node.value.toString(),
+                        reference: node.value.toString()
+                    });
+                }
+            }
+        });
+    }
+}
+
+export interface INavigableNode extends ITreeEntry {
+    level: number;
+    children: INavigableNode[];
+}
+
+function initPageNode(page: IPageData, level: number): INavigableNode {
+    return {
+        title: page.reference,
+        reference: page.reference,
+        level,
+        children: [],
+    };
+}
+
+function initHeaderNode(node: ITag, level: number): INavigableNode {
+    return {
+        title: node.value.toString(),
+        reference: node.value.toString(),
+        level,
+        children: [],
+    }
+}
+
+export function createNavigableTree(data: IDocumentalistData, page: IPageData, level = 0) {
+    const pageNode: INavigableNode = initPageNode(page, level);
+    if (page.contents != null) {
+        page.contents.forEach((node: ContentNode, i: number) => {
+            if (isTag(node)) {
+                if (node.tag == "page") {
+                    const subpage = data.docs.pages[node.value as string];
+                    pageNode.children.push(createNavigableTree(data, subpage, level + 1));
+                }
+                if (i != 0 && node.tag.match(/^#+$/)) {
+                    pageNode.children.push(initHeaderNode(node, level + 1));
+                }
+            }
+        });
+    }
+    return pageNode;
 }
---
+++ test/buildNav.js
@@ -0,0 +1,19 @@
+function readStdin(callback) {
+    let content = "";
+    process.stdin.setEncoding("utf-8");
+    process.stdin.resume();
+    process.stdin.on("data", (buf) => {
+        content += buf;
+    });
+    process.stdin.on("end", () => {
+        callback(content);
+    });
+}
+
+const { createNavigableTree } = require("../dist/client");
+
+readStdin((stdin) => {
+    const docs = JSON.parse(stdin);
+    const navigable = createNavigableTree(docs, docs.docs.pages.nav);
+    console.log(JSON.stringify(navigable, null, 2));
+});
