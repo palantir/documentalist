c9fdf456acad6f0412f0512fa89c2b417befb999
Bill Dwyer <bdwyer@palantir.com>
2017-02-23 16:49:42 -0800

    add demo script for PRs

---
+++ theme/demo.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const bot = require("circle-github-bot").create();
+
+demos = [
+    bot.artifactLink('docs/index.html', 'docs'),
+];
+
+bot.comment(`
+<h3>${bot.env.commitMessage}</h3>
+Preview: <strong>${demos.join(' | ')}</strong>
+`);
