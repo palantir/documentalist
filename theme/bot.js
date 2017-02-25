1a827ebd902a25a6e7ec5a2d9b2fb2213228105b
Gilad Gray <ggray@palantir.com>
2017-02-24 17:18:40 -0800

    small theme updates - prop docs under type, like blueprint. for maximal line length. - adjust padding so borders stretch the full width - tslint fixes + js mastery

--- theme/bot.js
+++ theme/bot.js
@@ -3,10 +3,10 @@
 const bot = require("circle-github-bot").create();
 
 demos = [
-    bot.artifactLink('docs/index.html', 'docs'),
+    bot.artifactLink("docs/index.html", "docs"),
 ];
 
 bot.comment(`
 <h3>${bot.env.commitMessage}</h3>
-Preview: <strong>${demos.join(' | ')}</strong>
+Preview: <strong>${demos.join(" | ")}</strong>
 `);
