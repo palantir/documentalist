ae3af2d45f4e450637187a4f6ffcb11efa89ca42
Gilad Gray <ggray@palantir.com>
2017-03-16 13:50:53 -0700

    move page utils to bottom

--- src/page.ts
+++ src/page.ts
@@ -10,32 +10,6 @@ import { IPageData, isTag } from "./client";
 
 export type PartialPageData = Pick<IPageData, "absolutePath" | "contentRaw" | "contents" | "metadata">;
 
-export function makePage(props: PartialPageData): IPageData {
-    const title = getTitle(props);
-    const reference = getReference(props);
-    return { ...props, reference, title };
-}
-
-function getReference(data: PartialPageData) {
-    if (data.metadata.reference != null) {
-        return data.metadata.reference;
-    }
-    return path.basename(data.absolutePath, path.extname(data.absolutePath));
-}
-
-function getTitle(data: PartialPageData) {
-    if (data.metadata.title !== undefined) {
-        return data.metadata.title;
-    }
-
-    const first = data.contents[0];
-    if (isTag(first) && first.tag.match(/^#+$/)) {
-        return first.value as string;
-    }
-
-    return "(untitled)";
-}
-
 export class PageMap {
     private pages: Map<string, IPageData> = new Map();
 
@@ -80,4 +54,30 @@ export class PageMap {
         }
         return object;
     }
+}
+
+function makePage(props: PartialPageData): IPageData {
+    const title = getTitle(props);
+    const reference = getReference(props);
+    return { ...props, reference, title };
+}
+
+function getReference(data: PartialPageData) {
+    if (data.metadata.reference != null) {
+        return data.metadata.reference;
+    }
+    return path.basename(data.absolutePath, path.extname(data.absolutePath));
+}
+
+function getTitle(data: PartialPageData) {
+    if (data.metadata.title !== undefined) {
+        return data.metadata.title;
+    }
+
+    const first = data.contents[0];
+    if (isTag(first) && first.tag.match(/^#+$/)) {
+        return first.value as string;
+    }
+
+    return "(untitled)";
 }
