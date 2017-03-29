b33df981301ae834829cde876ba722bf0ddb5bc8
Bill Dwyer <bdwyer@palantir.com>
2017-03-29 15:14:31 -0700

    Re-arrange interfaces

--- src/client/index.ts
+++ src/client/index.ts
@@ -11,35 +11,6 @@ export * from "./typescript";
 export * from "./utils";
 
 /**
- * A single Documentalist page, parsed from a single source file.
- */
-export interface IPageData extends IBlock {
-    /** Unique identifier for addressing this page. */
-    reference: string;
-
-    /** Fully qualified route to this page: slash-separated references of all parent pages. */
-    route: string;
-
-    /** Human-friendly title of this page. */
-    title: string;
-}
-
-/** An `@#+` tag belongs to a specific page. */
-export interface IHeadingNode extends INavigable {
-    /** Display title of page heading. */
-    title: string;
-}
-
-/** A page has ordered children composed of `@#+` and `@page` tags. */
-export interface IPageNode extends IHeadingNode {
-    /** Ordered list of pages and headings that appear on this page. */
-    children: Array<IPageNode | IHeadingNode>;
-
-    /** Unique reference of this page, used for retrieval from store. */
-    reference: string;
-}
-
-/**
  * The basic components of a navigable resource: a "route" at which it can be accessed and
  * its depth in the layout hierarchy. Heading tags and hierarchy nodes both extend this interface.
  */
@@ -118,4 +89,33 @@ export interface IBlock {
 
     /** Arbitrary YAML metadata parsed from front matter of source file, if any, or `{}`. */
     metadata: IMetadata;
+}
+
+/**
+ * A single Documentalist page, parsed from a single source file.
+ */
+export interface IPageData extends IBlock {
+    /** Unique identifier for addressing this page. */
+    reference: string;
+
+    /** Fully qualified route to this page: slash-separated references of all parent pages. */
+    route: string;
+
+    /** Human-friendly title of this page. */
+    title: string;
+}
+
+/** An `@#+` tag belongs to a specific page. */
+export interface IHeadingNode extends INavigable {
+    /** Display title of page heading. */
+    title: string;
+}
+
+/** A page has ordered children composed of `@#+` and `@page` tags. */
+export interface IPageNode extends IHeadingNode {
+    /** Ordered list of pages and headings that appear on this page. */
+    children: Array<IPageNode | IHeadingNode>;
+
+    /** Unique reference of this page, used for retrieval from store. */
+    reference: string;
 }
