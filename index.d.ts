88c16e1f01dd1b7e0b47c713055fdbef9bae6284
Adi Dahiya <adahiya@palantir.com>
2019-04-09 17:59:07 -0400

    Updates

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+interface IRoute {
+    route: string;
+    render: () => string;
+}
+declare class Router {
+    el: HTMLElement;
+    private defaultRoute;
+    private routes;
+    private currentRoute;
+    constructor(el: HTMLElement, defaultRoute?: string);
+    start(): void;
+    register(route: IRoute): void;
+    route(): void;
+}
+declare function queryAll(element: Element, selector: string): HTMLElement[];
+declare const nav: Element;
+declare function selectCurrent(route: string): void;
+declare const router: Router;
+declare const routables: HTMLElement[];
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2017-present Palantir Technologies, Inc. All rights reserved.
- * Licensed under the BSD-3 License as modified (the “License”); you may obtain
- * a copy of the license in the LICENSE and PATENTS files in the root of this
- * repository.
- */
-
-export * from "./documentalist";
-export * from "./plugins";
