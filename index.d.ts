7fd30371357088b55920557cb4493f13224c8632
Adi Dahiya <adahiya@palantir.com>
2022-04-05 12:53:37 -0400

    Updates

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * Route
+ */
 interface IRoute {
     route: string;
     render: () => string;
